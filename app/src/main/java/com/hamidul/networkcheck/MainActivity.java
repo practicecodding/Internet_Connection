a31ed73421868c12310fdee4047c3b7c20a9e2bf
user <practicecoddin@gmail.com>
2023-09-02 04:53:40 +0600

    Network Check

--- app/src/main/java/com/hamidul/networkcheck/MainActivity.java
+++ app/src/main/java/com/hamidul/networkcheck/MainActivity.java
@@ -3,12 +3,19 @@ package com.hamidul.networkcheck;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
