e5e302454a69febe83ab3fdbaf772b35e0f22b7a
user <practicecoddin@gmail.com>
2023-11-13 06:10:50 +0600

    Internet Connection

---
+++ app/src/main/java/com/hamidul/internetconnection/FirstFragment.java
@@ -0,0 +1,19 @@
+package com.hamidul.internetconnection;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FirstFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View myView = inflater.inflate(R.layout.fragment_first, container, false);
+
+        return myView;
+    }
+}
--- app/src/main/java/com/hamidul/internetconnection/MainActivity.java
+++ app/src/main/java/com/hamidul/internetconnection/MainActivity.java
@@ -1,14 +1,25 @@
 package com.hamidul.internetconnection;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         setContentView(R.layout.activity_main);
 
-        broadcastReceiver = new ConnectionReceiver();
+        broadcastReceiver = new ConnectionReceiverMain();
         registerNetworkBroadcast();
 
     }
@@ -46,5 +57,63 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
         unregisterNetwork();
     }
+
+    public class ConnectionReceiverMain extends BroadcastReceiver {
+        Context mContext;
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            if (isConnected(context)){
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.add(R.id.frameLayout,new FirstFragment());
+                fragmentTransaction.commit();
+            }else {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.add(R.id.frameLayout,new SecondFragment());
+                fragmentTransaction.commit();
+            }
+
+
+        }
+
+        public boolean isConnected(Context context){
+
+            try {
+                ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(context.CONNECTIVITY_SERVICE);
+                NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+                return (networkInfo!=null && networkInfo.isConnected());
+            }catch (NullPointerException e){
+                e.printStackTrace();
+                return false;
+            }
+
+
+        }
+
+        public void showDialog(){
+            AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+            LayoutInflater layoutInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View view = layoutInflater.inflate(R.layout.alertdialog_layout,null);
+            builder.setView(view);
+            Button buttonOk = view.findViewById(R.id.buttonOk);
+
+            final AlertDialog alertDialog = builder.create();
+
+
+            buttonOk.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    alertDialog.dismiss();
+                }
+            });
+            alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            alertDialog.show();
+
+        }
+
+    }
+
 
 }
